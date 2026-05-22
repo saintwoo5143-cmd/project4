990d24d56315c267d4709c4c9c7618d4ddb2c489
sungminLee <sungmin7955@gmail.com>
2026-05-22 16:38:12 +0900

    plusNavInHeader1638-sungmin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import '../App.css'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 function Header() {
+  const navigate = useNavigate()
+
   return (
     <header className="app-header">
-      <h1 className="app-title">Image Gallery</h1>
+      <h1 className="app-title" onClick={() => navigate('/')} style={{ cursor: 'pointer' }}>
+        Image Gallery
+      </h1>
       <nav className="header-nav">
         <NavLink to="/" className="nav-link">Home</NavLink>
         <NavLink to="/list" className="nav-link">List</NavLink>
