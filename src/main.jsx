fbcced0d918015fda2ca570676d2c6cac77f45c3
sungminLee <sungmin7955@gmail.com>
2026-05-22 15:56:35 +0900

    test1556-sungmin

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Home from './Home.jsx'
+import Create from './Create.jsx'
+import Update from './Update.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
